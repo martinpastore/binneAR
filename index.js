50747e6573cba48f68d7ac5bfdad2dfc9837aa77
E_MPASTORE <martin.pastore@incluit.com>
2018-09-10 21:17:45 -0300

    fixes

---
+++ index.js
@@ -0,0 +1,29 @@
+var list = require('./bins/bins.json');
+
+exports.checkBin = function (bin) {
+    bin = bin.replace(/ /g, '');
+    if (bin.length > 6) {
+        bin = bin.substring(0, 6);
+    }
+    return list[bin];
+}
+
+exports.validateBin = function (card) {
+    card = card.replace(/ /g, '');
+    var nCheck = 0, nDigit = 0, bEven = false;
+    card = card.replace(/\D/g, "");
+
+    for (var n = card.length - 1; n >= 0; n--) {
+        var cDigit = card.charAt(n),
+            nDigit = parseInt(cDigit, 10);
+
+        if (bEven) {
+            if ((nDigit *= 2) > 9) nDigit -= 9;
+        }
+
+        nCheck += nDigit;
+        bEven = !bEven;
+    }
+
+    return (nCheck % 10) == 0;
+}
